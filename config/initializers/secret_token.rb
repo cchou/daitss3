0c04c64198a8c2f9e8e8de6a4a9408fa8729c7fc
Carol Chou <cchou@ufl.edu>
2013-06-21 13:44:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daitss::Application.config.secret_token = '05d664c52c6c35d62cea0a895e591861942c7a31831aaaca86afb13a9d8fdee032003b4e29c818c884e8c7fd591e9478aeadb6a403caaffbe91ad3bd413d7fc6'
