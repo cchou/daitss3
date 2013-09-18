30e64cbdb83474aea3a9f24d2be73a7c965dbea7
Carol Chou <cchou@ufl.edu>
2013-09-18 14:10:54 -0400

    implement search

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -6,22 +6,81 @@ class PackagesController < ApplicationController
     # @packages = Package.paginate(page: params[:page])
     # http://stackoverflow.com/questions/12429429/datamapper-sorting-results-through-association
     # if (params[:sort] == "account")
- 
     # elsif (params[:sort] == "project")
-  
     # else
+    unless (params[:id_search].nil?)
       sort = DataMapper::Query::Operator.new(sort_column, sort_direction)
       @packages= Package.search(params[:id_search]).all(:order => [sort]).paginate(page: params[:page])
-    # end
+    else
+      # filter on activity
+      names = 
+        case params[:activity_search]
+        when 'submitted'
+          "submit"
+        when 'rejected'
+          ["reject","daitss v.1 reject"] 
+        when 'archived'
+          "ingest finished"
+        when 'disseminated'
+          "disseminate finished"
+        when 'error'
+          ["ingest snafu", "disseminate snafu", "refresh snafus"]
+        when 'withdrawn'
+          "withdraw finished"
+        else
+          ['submit', "reject", "ingest finished", "disseminate finished", "ingest snafu", "disseminate snafu", "withdraw finished", "daitss v.1 provenance"]
+        end
+
+      # filter on date range
+      start_date  = if params[:start_time_search] and !params[:start_time_search].strip.empty?
+          DateTime.strptime(params[:start_time_search], "%Y-%m-%dT%H:%M")
+        else
+          Time.at 0
+        end
+
+      end_date = if params[:end_time_search] and !params[:end_time_search].strip.empty?
+        DateTime.strptime(params[:end_time_search], "%Y-%m-%dT%H:%M")
+      else
+        DateTime.now
+      end
+
+      end_date += 1
+      range = (start_date..end_date)
+
+      # lookup account, project if passed in
+      if (params[:account] && params[:account]["account_id"])
+        account = Account.get(params[:account]["account_id"])
+        if params[:project] && params[:project] ["project_id"]
+          # account and project specified
+          project = account.projects.first(:id => params[:project]["project_id"])    
+          ps = project.packages.events.all(:timestamp => range, :name => names, :order => [ :timestamp.desc ]).packages 
+        else
+          # account but not project specified
+          ps = account.projects.packages.events.all(:timestamp => range, :name => names, :order => [ :timestamp.desc ] ).packages 
+        end
+      else 
+        # neither account nor project specified
+        ps = Event.all(:timestamp => range, :name => names, :order => [ :timestamp.desc ]).packages 
+      end
+
+      # filter on batches
+      batch = Batch.get(params[:batch_search])
+      if batch       
+        ps = ps.find_all { |p| p.batches.include? batch } 
+      end
+      @packages = ps.paginate(page: params[:page])
+    end
     @projects = []
   end
   
   # upload a package 
   def upload
-    # the actual file that was posted, a StringIO
+    # the actual file that was sent via HTTP post, a StringIO
     file = request.env['rack.input'] rescue nil
+
     # the name of the file passed in the HTTP header
     filename = request.env['HTTP_X_FILENAME']  
+
     # save the uploaded file into the tmp directory
     data = file.read
     dir = Dir.mktmpdir
