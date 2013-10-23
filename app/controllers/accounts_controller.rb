6a3e380ae5838a13cbf351c9f3ba83a45a64f877
Carol Chou <cchou@ufl.edu>
2013-10-23 13:57:00 -0400

    display a message about  foreign key constraint in delete account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -73,8 +73,12 @@ class AccountsController < ApplicationController
   # DELETE /accounts/1.json
   def destroy
     @account = Account.get(params[:id])
-    @account.destroy
-
+    if (@account.destroy)
+      flash[:success] = "Account destroyed."
+    else
+      flash[:error] = "Cannot delete the account, other database records still reference it"
+    end
+      
     respond_to do |format|
       format.html { redirect_to accounts_url }
       format.json { head :no_content }
