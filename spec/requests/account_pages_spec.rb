1e2afc89cc7ea3866cb4d1969371cccc271621c4
Carol Chou <cchou@ufl.edu>
2013-12-12 14:12:21 -0500

    account deletion

--- spec/requests/account_pages_spec.rb
+++ spec/requests/account_pages_spec.rb
@@ -11,8 +11,7 @@ describe "AccountPages" do
       Account.user_accounts.each do |account|
         expect(page).to have_selector('td', text: account.id)
       end
-    end      
-
+    end 
   end
 
   describe "create a new account" do
@@ -23,22 +22,23 @@ describe "AccountPages" do
       fill_in "Report email", with: "report@fda.edu"
       click_button "Create Account"        
     end
-
     it { should have_selector('p', text: 'Account was successfully created.') }
   end
 
-  # describe "delete an account" do
-  #    let(:account) { Account.get("ACT") }
-  #    
-  #    before do
-  #      visit accounts_path
-  #    end
-  #    it { should have_link('delete', href: accounts_path(account)) }
-  #    it "should be able to delete an account" do
-  #      expect do
-  #        click_link('delete', href: accounts_path(account))
-  #      end.to change(Account, :count).by(-1)
-  #    end
-  #    it { should_not have_link('delete', href: accounts_path(account)) }
-  #  end
+  describe "delete an account" do
+    let(:account) { Account.get("ACT") }
+     
+    before do
+      visit accounts_path
+    end
+    
+    it { should have_link('Destroy', href: accounts_path+'/ACT') }
+    it "should be able to delete an account" do
+      expect do
+        #click_link('Destroy', href: accounts_path+'/ACT')
+        page.find(:xpath, "//a[@href='#{accounts_path}/ACT']").click
+      end.to change(Account, :count).by(-1)
+    end
+    it { should_not have_link('Destroy', href: accounts_path+'/ACT') }
+  end
 end
