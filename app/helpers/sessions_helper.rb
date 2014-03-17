b25ec8cfe72b400919864d9ac6a0685d0c2d5baa
Carol Chou <cchou@ufl.edu>
2014-03-17 14:51:23 -0400

    log template

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,17 +10,17 @@ module SessionsHelper
     self.current_user = nil
     cookies.delete(:remember_token)
   end
-  
+
   def signed_in?
     puts "current user #{@current_user}"
     puts "cookies #{cookies[:remember_token]}"
     !current_user.nil?
   end
-    
+
   def current_user=(user)
     @current_user = user
   end
-  
+
   def current_user
     @current_user ||= User.first(:remember_token => cookies[:remember_token])
   end  
@@ -28,7 +28,7 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
-    
+
   def admin_user?
     current_user.is_admin_contact?
   end
