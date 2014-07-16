4d0e769b3acd0cb1dc275f3e645ede4045225dfd
Jon <jpitts@flvc.org>
2014-07-16 12:46:13 -0400

    Dashboard enhancements

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,16 @@
 module MainHelper
+  
+  def accounts
+    Account.user_accounts
+  end
+
+  def aip_data
+    DataMapper.repository(:default).adapter.select("SELECT project_account_id, SUM(size) AS size, COUNT(*) AS AIP_count, SUM(datafile_count) AS file_count FROM packages INNER JOIN accounts ON packages.project_account_id=accounts.id INNER JOIN aips ON packages.id=aips.package_id INNER JOIN copies ON aips.id=copies.aip_id GROUP BY project_account_id")
+  end
+  
+  def sip_data
+    DataMapper.repository(:default).adapter.select("SELECT project_account_id, SUM(size_in_bytes) AS size, COUNT(*) AS SIP_count, SUM(number_of_datafiles) AS file_count FROM packages INNER JOIN accounts ON packages.project_account_id=accounts.id INNER JOIN sips ON packages.id=sips.package_id GROUP BY project_account_id ORDER BY project_account_id")
+  end
+  
+  
 end
